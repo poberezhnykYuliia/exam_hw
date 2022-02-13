089000083dff5322ceba0fc9b86f4b65c49716bc
julia <juliapoberezhnuk@gmail.com>
2022-02-14 01:30:30 +0200

    almost ready

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems1 = document.querySelectorAll('.sidenav');
+    var instances1 = M.Sidenav.init(elems1, {
+      
+    });
+    // var instance = M.Sidenav.getInstance.open(elem);
+  });
