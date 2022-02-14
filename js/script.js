a303a899e4af1ec89d394a0ca00a3ebf54beabcd
julia <juliapoberezhnuk@gmail.com>
2022-02-14 14:01:12 +0200

    last changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems1 = document.querySelectorAll('.sidenav');
-    var instances1 = M.Sidenav.init(elems1, {
-      
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {
+     
+     
     });
-    // var instance = M.Sidenav.getInstance.open(elem);
+    
   });
